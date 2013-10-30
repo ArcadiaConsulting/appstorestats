38a81f46d7f27112165de8f213de60d411bc4ebc
Javier Gutierrez Alonso <jgutierrez@localhost.localdomain>
2013-10-30 09:58:23 +0100

    added ios implementation for units

--- appstoresstats-common/src/main/java/es/arcadiaconsulting/appstoresstats/common/IStoreStats.java
+++ appstoresstats-common/src/main/java/es/arcadiaconsulting/appstoresstats/common/IStoreStats.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public interface IStoreStats {
 	
-	public CommonStatsData getStatsForApp(String user,String password,String appId,Date initDate,Date endDate);
-	public List<CommonStatsData> getStatsForAllApps(String user,String password,Date initDate,Date endDate);
-	public CommonStatsData getFullStatsForApp(String user,String password,String appId);
-	public List<CommonStatsData> getFullStatsForAllApps(String user,String password);
+	public CommonStatsData getStatsForApp(String user,String password,String appId,Date initDate,Date endDate,String vendorId);
+	//public List<CommonStatsData> getStatsForAllApps(String user,String password,Date initDate,Date endDate);
+	public CommonStatsData getFullStatsForApp(String user,String password,String appId,String vendorId);
+	//public List<CommonStatsData> getFullStatsForAllApps(String user,String password);
 	
 }
