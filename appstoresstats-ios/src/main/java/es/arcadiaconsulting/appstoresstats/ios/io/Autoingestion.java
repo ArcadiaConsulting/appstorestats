ef0eff942b934a51a4b126920965d37e316b063e
jgalonso <jgutierrez@localhost.localdomain>
2013-11-03 23:53:57 +0100

    added

--- appstoresstats-ios/src/main/java/es/arcadiaconsulting/appstoresstats/ios/io/Autoingestion.java
+++ appstoresstats-ios/src/main/java/es/arcadiaconsulting/appstoresstats/ios/io/Autoingestion.java
@@ -1,9 +1,13 @@
 package es.arcadiaconsulting.appstoresstats.ios.io;
 
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -12,15 +16,20 @@ import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 import java.util.Vector;
+import java.util.zip.GZIPInputStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import es.arcadiaconsulting.appstoresstats.ios.model.AutoingestionBean;
+import es.arcadiaconsulting.appstoresstats.ios.model.Constants;
 import es.arcadiaconsulting.appstoresstats.ios.model.UnitData;
 
 public class Autoingestion
@@ -29,8 +38,12 @@ public class Autoingestion
   private static final Logger logger = LoggerFactory.getLogger(Autoingestion.class);
 	
   public static List<UnitData> getUnitsByDate(/**String propertiesFile,*/ String user,String password,String vendorId, String reportType, String dateType, String reportSubType, String date, String sku){
+	  
+	  
 	  try{
-		  InputStream is=	getSalesOutput(new String[]{/**propertiesFile,*/user,password,vendorId,reportType,dateType,reportSubType,date});
+		  
+		  
+		  List<AutoingestionBean> is=	getSalesOutput(new String[]{/**propertiesFile,*/user,password,vendorId,reportType,dateType,reportSubType,date});
 		  if(is==null){
 			  logger.error("Problem getting Autoingestion");
 			  return null;
@@ -47,7 +60,18 @@ public class Autoingestion
   
   public static String getAppleIDBySKU(/**String propertiesFile,*/ String user,String password,String vendorId, String reportType, String dateType, String reportSubType, String date, String sku) throws IOException{
 	  String line = new String();
-	  InputStreamReader salesOutputReader = new InputStreamReader(getSalesOutput(new String[]{/**propertiesFile,*/user,password,vendorId,reportType,dateType,reportSubType,date}));
+	  List<AutoingestionBean> salesOutputReader = getSalesOutput(new String[]{/**propertiesFile,*/user,password,vendorId,reportType,dateType,reportSubType,date});
+	  for (Iterator iterator = salesOutputReader.iterator(); iterator.hasNext();) {
+		AutoingestionBean autoingestionBean = (AutoingestionBean) iterator
+				.next();
+		if(autoingestionBean.getSku().equals(sku)){
+			 return autoingestionBean.getAppleIdentifier();
+		 }
+		
+	}
+	  return null;
+	   
+	  /**
  	  BufferedReader br = new BufferedReader(salesOutputReader);
 	  try {
 		 //discart first line 
@@ -70,12 +94,31 @@ public class Autoingestion
 		logger.error("getAppleIDBySKU Exception", e);
 		throw e;
 	}
+	*/
   }
   
   
-  public static List<UnitData> getUnits(InputStream salesOutput, String sku) throws IOException{
+  public static List<UnitData> getUnits(List<AutoingestionBean> salesOutput, String sku) throws IOException{
 	  List<UnitData> unitDataList = new Vector<UnitData>();
-	  String line = new String();
+	  
+	  for (Iterator iterator = salesOutput.iterator(); iterator.hasNext();) {
+		AutoingestionBean autoingestionbean = (AutoingestionBean) iterator.next();
+		boolean isupdate = false;
+		for (int i = 0; i < Constants.UPDATE_PRODUCT_TYPE_IDENTIFYER.length; i++) {
+			if(Constants.UPDATE_PRODUCT_TYPE_IDENTIFYER[i].equals(autoingestionbean.getProductTypeIdentifier())){
+				isupdate = true;
+				break;
+			}			
+		}
+		
+		if(autoingestionbean.getSku().equals(sku)&& !isupdate){
+			 unitDataList.add(new UnitData(autoingestionbean.getCountryCode(),autoingestionbean.getUnits()));
+		 }
+		
+	}
+	 
+	  
+	  /**
 	  InputStreamReader salesOutputReader = new InputStreamReader(salesOutput);
  	  BufferedReader br = new BufferedReader(salesOutputReader);
 	  try {
@@ -92,20 +135,22 @@ public class Autoingestion
 				 unitDataList.add(new UnitData(lineElements[13],Integer.getInteger(lineElements[7]).intValue()));
 			 }
 		}
+		
 		return unitDataList;
 		
 	} catch (IOException e) {
 		logger.error("getUnits Exception", e);
 		throw e;
 	}
-
+		*/
+	  return unitDataList;
 	 
 	  
   }
   
   
 	
-  private static InputStream getSalesOutput(String[] paramArrayOfString)
+  private static List<AutoingestionBean> getSalesOutput(String[] paramArrayOfString)
   {
     int i = 0;
     String str1 = null;
@@ -247,29 +292,132 @@ public class Autoingestion
   }
 
   //private static void getFile(HttpURLConnection paramHttpURLConnection) throws IOException
-  private static InputStream getFile(HttpURLConnection paramHttpURLConnection) throws IOException
+  private static List<AutoingestionBean> getFile(HttpURLConnection paramHttpURLConnection) throws IOException
   {
-    String str = paramHttpURLConnection.getHeaderField("filename");
-    logger.info(str);
- //   int i = 0;
+  //  String str = paramHttpURLConnection.getHeaderField("filename");
+	 
+    //logger.info(str);
+   int i = 0;
 
-    	return paramHttpURLConnection.getInputStream();
+    	//return paramHttpURLConnection.getInputStream();
     
- //   BufferedInputStream localBufferedInputStream = new BufferedInputStream(paramHttpURLConnection.getInputStream());
-
-/*    BufferedOutputStream localBufferedOutputStream = new BufferedOutputStream(new FileOutputStream(str));
+    //BufferedInputStream localBufferedInputStream = new BufferedInputStream(paramHttpURLConnection.getInputStream());
+    
+    try {
+		String fileString = decompress(paramHttpURLConnection.getInputStream());
+		return buildAutoingestionTable(fileString.split("\n"));
+	} catch (Exception e) {
+		logger.error("problem  decommpressing file",e );
+		return null;
+		
+	}
+    
+    /**
+    BufferedOutputStream localBufferedOutputStream = new BufferedOutputStream(new FileOutputStream(str));
 
     byte[] arrayOfByte = new byte[1024];
 
     while ((i = localBufferedInputStream.read(arrayOfByte)) != -1) {
       localBufferedOutputStream.write(arrayOfByte, 0, i);
     }
-*/
- //   localBufferedInputStream.close();
-//    localBufferedOutputStream.close();
 
-//   System.out.println("File Downloaded Successfully ");
+   localBufferedInputStream.close();
+    localBufferedOutputStream.close();
+
+  System.out.println("File Downloaded Successfully ");
     
-    
+  return null;
+  */
   }
+  
+  public static String decompress(InputStream input) throws Exception {
+	  // opens the compressed file
+	  GZIPInputStream in = new GZIPInputStream(input);
+	  ByteArrayOutputStream output = new ByteArrayOutputStream();
+	  // Transfer bytes from the compressed file to the output file
+	  byte[] buffer = new byte[1024];
+	  int len;
+	  while ((len = in.read(buffer)) > 0) {
+	    output.write(buffer, 0, len);
+	  }
+	  in.close();
+	  output.close();
+	  return output.toString();
+	}
+  
+  
+  public static List<AutoingestionBean> buildAutoingestionTable(String[] autoingestionTable){
+	  List<AutoingestionBean> autoingestionBean = new Vector<AutoingestionBean>();
+	  
+	  for (int i = 1; i < autoingestionTable.length; i++) {
+		autoingestionBean.add(buildAutogestionBean(autoingestionTable[i].trim().split("\t")));
+	  }
+	  
+	  return autoingestionBean;
+	  
+  }
+  
+  
+  public static AutoingestionBean buildAutogestionBean(String[] autoingestionLine){
+		AutoingestionBean bean = new AutoingestionBean();
+	  	
+		
+	  bean.setProvider(autoingestionLine[0]);
+		
+	  bean.setProviderCountry(autoingestionLine[1]);
+		
+	  bean.setSku(autoingestionLine[2]);
+		
+	  bean.setDeveloper(autoingestionLine[3]);
+		
+	  bean.setTitle(autoingestionLine[4]);
+		
+	  bean.setVersion(autoingestionLine[5]);
+		
+	  bean.setProductTypeIdentifier(autoingestionLine[6]);
+		
+	  bean.setUnits(Integer.valueOf(autoingestionLine[7]).intValue());
+		
+	  bean.setDeveloperProceds(Integer.valueOf(autoingestionLine[8]).intValue());
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+		
+		try {
+			bean.setBeginDate(sdf.parse(autoingestionLine[9]));
+		} catch (ParseException e) {
+			logger.error("Build AutoingestionBean error, Incorrect beginDate format",e);
+		}
+		
+		
+		try{
+		bean.setEndDate(sdf.parse(autoingestionLine[10]));
+		}catch(ParseException e) {
+			logger.error("Build AutoingestionBean error, Incorrect endDate format",e);
+		}
+		bean.setCustomerCurrency(autoingestionLine[11]);
+		
+		bean.setCountryCode(autoingestionLine[12]);
+		
+		bean.setCurrencyOfProcess(autoingestionLine[13]);
+		
+		bean.setAppleIdentifier(autoingestionLine[14]);
+		
+		bean.setCustomerPrice(Float.valueOf(autoingestionLine[15]).floatValue());
+		
+		if(autoingestionLine.length>16)
+			bean.setPromoCode(autoingestionLine[16]);
+		
+		if(autoingestionLine.length>17)
+			bean.setParentIdentifier(autoingestionLine[17]);
+		
+		if(autoingestionLine.length>18)
+		bean.setSubscription(autoingestionLine[18]);
+		
+		if(autoingestionLine.length>19)
+		bean.setPeriod(autoingestionLine[19]);
+		
+		return bean;
+	}
+  
+  
 }
