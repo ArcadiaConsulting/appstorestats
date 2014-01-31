ed51b872aaa3e23f07ab66ff7a81cb8cc9447ff9
Gonzalo Gómez García <gonzalo@arcadiaconsulting.es>
2014-01-31 03:34:35 +0100

    Fixed unit tests Sille line breaks

--- appstoresstats-ios/src/test/java/es/arcadiaconsulting/appstoresstats/ios/io/RSSHelperTest.java
+++ appstoresstats-ios/src/test/java/es/arcadiaconsulting/appstoresstats/ios/io/RSSHelperTest.java
@@ -9,7 +9,7 @@ public class RSSHelperTest extends TestCase{
 
 	public void testGetItunesURL(){
 		assertEquals("https://itunes.apple.com/es/app/fiestas-sampedros-burgos/id662257575?mt=8&uo=2",RSSHelper.getItunesURL("662257575"));
-		assertNull(RSSHelper.getItunesURL("0"));
+		assertNull(RSSHelper.getItunesURL("731374663"));
 	}
 	
 	public void testGetItunesRating(){
@@ -19,7 +19,7 @@ public class RSSHelperTest extends TestCase{
 		assertEquals(5,((Rating)rating.get(0)).getRate());
 		
 		assertEquals(5,rating.size());
-		assertNull(RSSHelper.getItunesRating("0"));
+		assertNull(RSSHelper.getItunesRating("731374663"));
 	}
 	
 }
